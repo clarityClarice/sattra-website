c13929d61d0165b80e34d6fd94ca0d53b234f22f
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-09 15:02:02 -0300

    Footer melhorado e teste de display de conteúdo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,4 +5,27 @@ export default props =>
     <Main title="Home">
         <div className="display-4">Bem-vindo!</div>
         <p className="mb-0" > Inserir conteúdo aqui</p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        <p className="mb-0"> Teste </p>
+        
     </Main>
