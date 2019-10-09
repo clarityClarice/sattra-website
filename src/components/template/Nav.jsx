08b6587e705609d8aee834c983ddb245743d0c9b
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-09 15:10:42 -0300

    Cor e responsividade
    Alterando cor do header para o laranja utilizado pela logo da empresa.
    
    Retirando icones para melhorar responsividade do Nav em celulares.

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 export default props =>
     <div className="menu-area">
         <nav className="menu">
-            <Link to="/"> <i className="fa fa-home"></i> Home </Link>
-            <Link to="/empresa"><i className="fa fa-building"></i> Empresa </Link>
-            <Link to="/produtos"><i className="fa fa-cog"></i> Produtos </Link>
-            <Link to="/contato"><i className="fa fa-comments"></i> Fale Conosco </Link>
+            <Link to="/"> Home </Link>
+            <Link to="/empresa"> Empresa </Link>
+            <Link to="/produtos"> Produtos </Link>
+            <Link to="/contato"> Fale Conosco </Link>
         </nav>
     </div>
