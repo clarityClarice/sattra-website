8e1a58adecfdf1f3eec8795606a81e5c23719198
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-10 09:43:56 -0300

    Atualizando contato e Infos sobre a empresa

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
 import Main from '../template/Main'
+import './Contato.css'
+
+//contato@sattra.com.br
+//+55 47 2125-0087
+//itajai-sc
+//fazer sessão para enviar mensagem
+
+function enviar(){
+
+}
+
 
 export default props =>
     <Main title="Fale Conosco">
+        <div className="content">
+
+            <div class="column1">
+                <p className="mb-0" > <i class="fa fa-map-marker" aria-hidden="true"></i> Itajaí - Santa Catarina</p>
+                <p />
+                <p className="mb-0" > <i class="fa fa-envelope" aria-hidden="true"></i> contato@sattra.com.br</p>
+                <p/>
+                <p class="mb-0"><i class="fa fa-phone" aria-hidden="true"></i> +55 47 2125-0087</p>
+                < p/>
+            </div>
+
+            <div className="column2">
+                
+            </div>
+
+        </div>
     </Main>
--- src/components/Empresa/Empresa.jsx
+++ src/components/Empresa/Empresa.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import Main from '../template/Main'
+import './Empresa.css'
 
 export default props =>
     <Main title="Empresa" >
-        Detalhes sobre a empresa
+        <div className="content">
+            <h1>
+                Sobre nós
+            </h1>
+            Somo uma empresa brasileira que oferece soluções inteligentes, especificas ou customizadas. Realizamos projetos e consultoria para portos, terminais alfandegados, não alfandegados, industrias e comércio em geral. Oferecemos sistemas de segurança em vídeo (CFTV) e câmeras IP, sistema de identificação de placas, contêineres e sistema de controle de acesso web. Atuamos com sistemas de segurança dos melhores e maiores fabricantes mundiais. Possuímos equipe de desenvolvimento próprio para controle de acesso e sistema de identificação de placas e containers. Inovação e melhoria contínua em todas as nossas soluções. 
+            <p/>
+            <h2>
+                Missão
+            </h2>
+            Fornecer ao mercado corporativo soluções robustas de monitoramento em vídeo, controle de acesso e leitura veicular ANPR/LPR e ACCR (OCR de placas, containers e vagões) e satisfazer as necessidades de nossos clientes, com ética, transparência, qualidade e inovação tecnológica, oferecendo sempre produtos e serviços com excelência e qualidade.
+            <p />
+            <h2>
+                Visão 
+            </h2>
+            Ser reconhecida como referência no seu segmento, destacando-se pelo profissionalismo, inovação, e satisfação de seus clientes.
+            < p/>
+            <h2>
+               Valores 
+            </h2>
+            Comprometimento, transparência, competência, qualidade, respeito e ética.
+        </div>
     </Main>
