f772f90b93c89785ce982b2cd0a3d4d38ed82b15
Cecília Clarice <clarity.clarice@gmail.com>
2019-10-10 13:44:09 -0300

    Pequenos ajustes e acréscimo de video

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import Main from '../template/Main'
-
+import Sattra from '../../Assets/Videos/Sattra.mp4'
+import './Home.css'
 
 function Home(){
     
     return(
         <Main title="Home">
             <div className="display-4">Bem-vindo!</div>
+            <div className="video">
 
-            <p className="mb-0" > Inserir conteúdo aqui</p>
+                <div className="videoContainer">
+                    <video autoplay controls>
+                        <source  src={Sattra} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video> 
+                </div>
+                
+                <div className="videoContainer">
+                    <video  controls>
+                        <source src={Sattra} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video> 
+                </div>
+                
+                
+            </div>
         </Main>
     )
 }
